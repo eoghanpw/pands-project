ce54a743bd2f03c2f0b1ae4aa8b22ffdbcad1131
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-05-18 13:18:11 +0100

    cleaning up code. adding jupyter notebook.

--- analysis.py
+++ analysis.py
@@ -25,21 +25,20 @@ iris = pd.read_csv("iris.csv", names=column_names)
 # Create the filename.
 FILENAME = "iris_variable_summary.txt"
 
-# Generate summary statistics for each variable.
-# Reference:
-# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html.
-summary = iris.describe()
-
-# Convert to string so it can be written to a text file.
-# Reference:
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html.
-summary_csv = summary.to_csv()
-
 
 # Function to write the summary stats to a text file.
 def variable_summary():
+
+    # Generate summary statistics for each variable and convert to string
+    # so it can be written to a text file.
+    # References:
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html.
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html.
+    summary = iris.describe().to_csv()
+
+    # Write summary statistics to txt file.
     with open(FILENAME, "w", newline="") as f:
-        f.write(summary_csv)
+        f.write(summary)
 
 
 # Run the function.
@@ -48,117 +47,137 @@ variable_summary()
 
 # 2. SAVE A HISTOGRAM OF EACH VARIABLE TO PNG FILES.
 
-# List of variables for the histograms.
-variables_hist = ("sepal_length_cm", "sepal_width_cm", "petal_length_cm",
-                  "petal_width_cm")
+# Function to save histograms to png files.
+def histogram_png():
 
-# Subsets of the data set by class.
-setosa_hist = iris["class"] == "Iris-setosa"
-versicolor_hist = iris["class"] == "Iris-versicolor"
-virginica_hist = iris["class"] == "Iris-virginica"
+    # List of variables for the histograms.
+    variables_hist = ("sepal_length_cm", "sepal_width_cm", "petal_length_cm",
+                      "petal_width_cm")
 
-# For loop to create histogram for each variable.
-for variable in variables_hist:
+    # Subsets of the data set by class.
+    setosa_hist = iris["class"] == "Iris-setosa"
+    versicolor_hist = iris["class"] == "Iris-versicolor"
+    virginica_hist = iris["class"] == "Iris-virginica"
 
-    # Use matplotlib subplots to create the histogram.
-    fig, ax = plt.subplots()
-
-    # Select plot type, data, label, bar colour, transparency.
-    ax.hist(iris[setosa_hist][variable], label="Setosa",
-            color="tab:green", alpha=0.5)
-
-    ax.hist(iris[versicolor_hist][variable], label="Versicolor",
-            color="tab:orange", alpha=0.5)
-
-    ax.hist(iris[virginica_hist][variable], label="Virginica",
-            color="tab:blue", alpha=0.5)
-
-    # Set the axis labels.
-    x_label_hist = variable.replace("_", " ").replace("cm", "(in centimetres)")
-    ax.set_xlabel(x_label_hist)
-    ax.set_ylabel("frequency")
-
-    # Set the title.
-    title_hist = variable.replace("_", " ").replace(" cm", "s").title()
-    ax.set_title(f"Iris {title_hist} per Class")
-
-    # Add legend.
-    ax.legend()
-
-    # Save each histogram as png file.
-    plt.savefig(f"{variable}_hist.png")
-
-    # Close each figure.
-    plt.close()
-
-
-# 3. OUTPUT A SCATTER PLOT OF EACH PAIR OF VARIABLES.
-
-# seaborn pairplot.
-
-# Dictionary of colours for the pairplots.
-colours_pairplt = {"Iris-setosa": "tab:green", "Iris-versicolor": "tab:orange",
-                   "Iris-virginica": "tab:blue"}
-
-# Use seaborn pairplot to generate one figure containing all scatter plots
-# for each pair of variables.
-# Adapted from:
-# https://seaborn.pydata.org/generated/seaborn.pairplot.html#seaborn-pairplot.
-# https://stackoverflow.com/a/47200170
-sns.pairplot(iris, hue="class", palette=colours_pairplt,
-             plot_kws={"alpha": 0.5})
-
-# Show plot.
-plt.show()
-
-# matplotlib.pyplot plot.
-
-# List of variables for the plots.
-variables_plot = ("sepal_length_cm", "sepal_width_cm", "petal_length_cm",
-                  "petal_width_cm")
-
-# Subsets of the data set by class.
-setosa_plot = iris["class"] == "Iris-setosa"
-versicolor_plot = iris["class"] == "Iris-versicolor"
-virginica_plot = iris["class"] == "Iris-virginica"
-
-# Nested for loop to run through each pair of variables with
-# continue statement to skip when x equals y. Will generate separate
-# figure for each scatter plot.
-# Reference: https://www.w3schools.com/python/python_for_loops.asp
-for x_var in variables_plot:
-    for y_var in variables_plot:
-        if x_var == y_var:
-            continue
+    # For loop to create histogram for each variable.
+    for var in variables_hist:
 
         # Use matplotlib subplots to create the histogram.
         fig, ax = plt.subplots()
 
-        # Select plot type, x & y data, marker shape, label, colour.
-        ax.plot(iris[setosa_plot][x_var], iris[setosa_plot][y_var],
-                "o", label="setosa", color="tab:green", alpha=0.5)
+        # Select plot type, data, label, bar colour, transparency.
+        ax.hist(iris[setosa_hist][var], label="Setosa",
+                color="tab:green", alpha=0.5)
 
-        ax.plot(iris[versicolor_plot][x_var], iris[versicolor_plot][y_var],
-                "o", label="versicolor", color="tab:orange", alpha=0.5)
+        ax.hist(iris[versicolor_hist][var], label="Versicolor",
+                color="tab:orange", alpha=0.5)
 
-        ax.plot(iris[virginica_plot][x_var], iris[virginica_plot][y_var],
-                "o", label="virginica", color="tab:blue", alpha=0.5)
+        ax.hist(iris[virginica_hist][var], label="Virginica",
+                color="tab:blue", alpha=0.5)
 
         # Set the axis labels.
-        ax.set_xlabel(x_var)
-        ax.set_ylabel(y_var)
+        x_label_hist = var.replace("_", " ").replace("cm", "(in centimetres)")
+        ax.set_xlabel(x_label_hist)
+        ax.set_ylabel("frequency")
 
         # Set the title.
-        x_title_plot = x_var.replace("_", " ").replace(" cm", "").title()
-        y_title_plot = y_var.replace("_", " ").replace(" cm", "").title()
-
-        ax.set_title(f"{x_title_plot} vs {y_title_plot} per Class")
+        title_hist = var.replace("_", " ").replace(" cm", "s").title()
+        ax.set_title(f"Iris {title_hist} per Class")
 
         # Add legend.
         ax.legend()
 
-        # Show scatter plots.
-        plt.show()
+        # Save each histogram as png file.
+        plt.savefig(f"{var}_hist.png")
+
+        # Close each figure.
+        plt.close()
+
+
+# Run the function.
+histogram_png()
+
+
+# 3. OUTPUT A SCATTER PLOT OF EACH PAIR OF VARIABLES.
+
+# Funtion to output seaborn pairplot.
+def iris_pairplot():
+
+    # Dictionary of colours for the pairplot.
+    colours_pairplot = {"Iris-setosa": "tab:green",
+                        "Iris-versicolor": "tab:orange",
+                        "Iris-virginica": "tab:blue"}
+
+    # Filter warnings due to bug:
+    # https://github.com/matplotlib/matplotlib/issues/26290.
+    import warnings
+    warnings.filterwarnings('ignore')
+
+    # Use seaborn pairplot to generate one figure containing all scatter plots
+    # for each pair of variables.
+    # Adapted from:
+    # https://seaborn.pydata.org/generated/seaborn.pairplot.html#seaborn-pairplot.
+    # https://stackoverflow.com/a/47200170
+    sns.pairplot(iris, hue="class", palette=colours_pairplot,
+                 plot_kws={"alpha": 0.5})
+
+    # Show plot.
+    plt.show()
+
+
+# Run the function.
+iris_pairplot()
+
+
+# Funtion to output matplotlib.pyplot plots.
+def iris_plots():
+
+    # List of variables for the plots.
+    variables_plot = ("sepal_length_cm", "sepal_width_cm",
+                      "petal_length_cm", "petal_width_cm")
+
+    # Subsets of the data set by class.
+    setosa_plot = iris["class"] == "Iris-setosa"
+    versicolor_plot = iris["class"] == "Iris-versicolor"
+    virginica_plot = iris["class"] == "Iris-virginica"
+
+    # Nested for loop to run through each pair of variables with
+    # continue statement to skip when x equals y. Will generate separate
+    # figure for each scatter plot.
+    # Reference: https://www.w3schools.com/python/python_for_loops.asp
+    for x_var in variables_plot:
+        for y_var in variables_plot:
+            if x_var == y_var:
+                continue
+            # Use matplotlib subplots to create the histogram.
+            fig, ax = plt.subplots()
+
+            # Select plot type, x & y data, marker shape, label, colour.
+            ax.plot(iris[setosa_plot][x_var], iris[setosa_plot][y_var],
+                    "o", label="setosa", color="tab:green", alpha=0.5)
+
+            ax.plot(iris[versicolor_plot][x_var], iris[versicolor_plot][y_var],
+                    "o", label="versicolor", color="tab:orange", alpha=0.5)
+
+            ax.plot(iris[virginica_plot][x_var], iris[virginica_plot][y_var],
+                    "o", label="virginica", color="tab:blue", alpha=0.5)
+            # Set the axis labels.
+            ax.set_xlabel(x_var)
+            ax.set_ylabel(y_var)
+
+            # Set the title.
+            x_title_plot = x_var.replace("_", " ").replace(" cm", "").title()
+            y_title_plot = y_var.replace("_", " ").replace(" cm", "").title()
+            ax.set_title(f"{x_title_plot} vs {y_title_plot} per Class")
+            # Add legend.
+            ax.legend()
+
+            # Show scatter plots.
+            plt.show()
+
+
+# Run the function.
+iris_plots()
 
 
 # 4. FURTHER ANALYSIS
